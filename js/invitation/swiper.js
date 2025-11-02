f45870209c4ec901d5cd94026a7ff6005a630e07
rottenraspberry <149756277+rottenraspberry@users.noreply.github.com>
2025-11-02 19:09:43 +0700

    Create swiper.js

---
+++ js/invitation/swiper.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false
+  },
+});
+
+Fancybox.bind('[data-fancybox]', {
+  Carousel: {
+    transition: "slide",
+  }
+});
